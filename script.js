ff3919c669ef3ec17f04a0055ba8a8fd5c0cef2b
Raissa Santos <raissa@Raissas-MacBook-Pro.local>
2023-09-04 14:26:37 -0700

    item 3

--- script.js
+++ script.js
@@ -7,16 +7,26 @@ colorPaletteUnit[0].style.backgroundColor = '#9400d3';
 colorPaletteUnit[1].style.backgroundColor = '#006400';
 colorPaletteUnit[2].style.backgroundColor = '#0000cd';
 colorPaletteUnit[3].style.backgroundColor = '#ff1493';
+let currentlySelected;
 
-const pixels = () => {
+const pixel = () => {
     const quadroPixels = document.querySelector('.quadroPixels');
 
-    for (let index =0; index < 25; index += 1){
-        const pixels =  document.createElement('div');
-        pixels.classList.add('pixels');
+    for (let index = 0; index < 25; index += 1) {
+        const pixel = document.createElement('div');
+        pixel.classList.add('pixel');
         // pixels.innerText = pixels();
-        quadroPixels.appendChild(pixels);
+        quadroPixels.appendChild(pixel);
     }
 }
+pixel();
 
-pixels();
+function changeClass(selectedColor){
+
+    if (currentlySelected !== undefined) {
+        currentlySelected.classList.remove('selected');    
+    }
+    selectedColor.classList.add('selected');
+    currentlySelected = selectedColor;
+
+}
